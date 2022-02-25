ee6d316b07aaa10aa1b2859b7db0e8b10140e535
Coding2Coding <76713830+Coding2Coding@users.noreply.github.com>
2022-02-24 19:54:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var leftWristXCoordinate = 0;
+var leftWristYCoordinate = 0;
+var rightWristXCoordinate = 0;
+var rightWristYCoordinate = 0;
+var isLeftWristOnTheCanvas = 0;
+var songStatus = "";
+var song2Status = "";
+var song2 = "";
+var song4 = "";
+
+function preload() {
+    song2 = loadSound("music.mp3");
+    song4 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNetModel = ml5.poseNet(video, modelLoaded);
+    poseNetModel.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet model is working");
+}
+
+function draw() {
+    image(video, 0, 0, 400, 400);
+
+    songStatus = song2.isPlaying();
+
+    fill("#69d7ff");
+    stroke("ffc0ff");
+
+    if(isLeftWristOnTheCanvas > 0.2) {
+        circle(leftWristXCoordinate, leftWristYCoordinate, 14);
+        song4.stop();
+        if(songStatus == false) {
+            song2.play();
+            document.getElementById("songName").innerHTML = "Song 1";
+        }
+    }
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        isLeftWristOnTheCanvas = results[0].pose.keypoints[9].score;
+        leftWristXCoordinate = results[0].pose.leftWrist.x;
+        leftWristYCoordinate = results[0].pose.leftWrist.y;
+        rightWristXCoordinate = results[0].pose.rightWrist.x;
+        rightWristYCoordinate = results[0].pose.rightWrist.y;
+    }
+}
